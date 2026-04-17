c58cd7bae6432a2ba67fda373e41e3a1aba11949
tolufem22@gmail.com <tolufem22@gmail.com>
2026-04-17 03:10:29 +0000

    Add daily locker activity fact model

--- models/fct_daily_locker_activity.sql
+++ models/fct_daily_locker_activity.sql
@@ -1,9 +1,9 @@
 select
     cast(event_timestamp as date) as event_date,
     locker_id,
-    locker_location_name,
+    max(locker_location_name) as locker_location_name,
     count(*) as total_events,
     count(distinct customer_id) as unique_customers,
     count(distinct order_id) as unique_orders
 from {{ ref('stg_amazon_locker') }}
-group by 1,2,3
+group by 1,2
